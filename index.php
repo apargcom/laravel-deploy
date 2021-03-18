02b802055b83f61b8110ca870c9dd7f82a57843d
Arsen Pyuskyulyan <arspyus@gmail.com>
2021-03-18 12:24:36 +0400

    Removed password from srouce

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?
 
-if($_GET['pass'] == 'wdeDReEe8tNAJWY6wCT9')
+if($_GET['pass'] == '') //Set password for accessing this URL
 {
 	$command = $_GET['force'] == 'true' ? './deploy.sh -f' : './deploy.sh';
 	echo '<pre>' . shell_exec($command) . '</pre>';
